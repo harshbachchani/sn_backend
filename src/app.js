12ec514ce33923285c152ecd1f09040a46335136
harshbachchani <harshbachchani9900@gmail.com>
2024-06-21 11:21:38 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-    methods: "GET,POST,PUT,DELETE,PATCH",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 
