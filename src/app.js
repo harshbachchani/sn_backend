574d57f62cfb270f9810fbc954877d183b008e01
harshbachchani <harshbachchani9900@gmail.com>
2024-06-24 11:50:08 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ import express from "express";
 import cors from "cors";
 const app = express();
 
+console.log("Starting app and setting CORS...");
 app.use(
   cors({
-    origin: "*",
+    origin: "https://sn-frontend-user.vercel.app",
   })
 );
+app.use((req, res, next) => {
+  console.log(`Received request for ${req.method} ${req.url}`);
+  next();
+});
 
 app.use(
   express.json({
@@ -17,6 +22,12 @@ app.use(
 app.use(express.urlencoded({ extended: true, limit: "16KB" }));
 app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    console.log("CORS headers:", res.get("Access-Control-Allow-Origin"));
+  });
+  next();
+});
 import adminRouter from "./routes/admin.routes.js";
 import userRouter from "./routes/user.routes.js";
 app.use("/api/v1/admin", adminRouter);
